637f3c15bfdea2339eba55357a1619ef4c861d30
Dharani-TM <dharani.thirumalai.m@gmail.com>
2026-07-22 23:47:41 +0530

    Max Pro of 3 Num

---
+++ Day5.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Day5{
+
+    public static int maximumProduct(int[] nums) {
+
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        int product1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
+
+        int product2 = nums[0] * nums[1] * nums[n - 1];
+
+        return Math.max(product1, product2);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println(maximumProduct(nums));
+
+        sc.close();
+    }
+}
