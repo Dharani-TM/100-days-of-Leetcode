3657eabb026d03ccf426f494223abe2c028c51eb
Dharani-TM <dharani.thirumalai.m@gmail.com>
2026-07-23 07:40:32 +0530

    Decode Ways

---
+++ Day6.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Day6{
+
+    public static int numDecodings(String s) {
+
+        int n = s.length();
+
+        if (s.charAt(0) == '0')
+            return 0;
+
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+
+            int oneDigit = s.charAt(i - 1) - '0';
+
+            int twoDigits = Integer.parseInt(s.substring(i - 2, i));
+
+            if (oneDigit >= 1)
+                dp[i] += dp[i - 1];
+
+            if (twoDigits >= 10 && twoDigits <= 26)
+                dp[i] += dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        System.out.println(numDecodings(s));
+
+        sc.close();
+    }
+}
