104fedc90eed56f62eb57060fc1c9220ca53a849
Dharani-TM <dharani.thirumalai.m@gmail.com>
2026-08-19 23:23:01 +0530

    128.Longest Consecutive Sequence

---
+++ Day33.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Day33 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int longest = 0;
+
+        for (int num : set) {
+            if (!set.contains(num - 1)) {
+                int current = num;
+                int count = 1;
+
+                while (set.contains(current + 1)) {
+                    current++;
+                    count++;
+                }
+
+                longest = Math.max(longest, count);
+            }
+        }
+
+        System.out.println(longest);
+
+        sc.close();
+    }
+}
