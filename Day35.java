d4a1575fa045e3674a3187ddbc3646325c150d89
Dharani-TM <dharani.thirumalai.m@gmail.com>
2026-08-20 21:55:12 +0530

    3069. Distribute Elements Into Two Arrays I

---
+++ Day35.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Day35 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        ArrayList<Integer> arr2 = new ArrayList<>();
+
+        arr1.add(nums[0]);
+        arr2.add(nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            int last1 = arr1.get(arr1.size() - 1);
+            int last2 = arr2.get(arr2.size() - 1);
+
+            if (last1 > last2) {
+                arr1.add(nums[i]);
+            } else {
+                arr2.add(nums[i]);
+            }
+        }
+
+        int[] result = new int[n];
+        int index = 0;
+
+        for (int num : arr1) {
+            result[index++] = num;
+        }
+
+        for (int num : arr2) {
+            result[index++] = num;
+        }
+
+        System.out.println(Arrays.toString(result));
+
+        sc.close();
+    }
+}
